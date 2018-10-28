a431c7270239cfceb671c97aeb6a00fa03c27fa9
AmraSelimovic <44226479+AmraSelimovic@users.noreply.github.com>
2018-10-28 11:55:43 +0100

    Update Knjiga.java

--- src/Knjiga.java
+++ src/Knjiga.java
@@ -3,7 +3,7 @@ public class Knjiga extends Racun {
 
 	private int brojKnjige;
 	private String imeKnjige;
-	private boolean statusKnjige=true; //false-izdata, true-slobodna
+	private boolean statusKnjige; //false-izdata, true-slobodna
 	
 	protected static ArrayList <Knjiga> listaKnjiga = new ArrayList<Knjiga>();
 	
@@ -45,38 +45,54 @@ public class Knjiga extends Racun {
 	public static boolean stanjeKnjige(int brojKnjige) {
 		for (int i = 0; i < listaKnjiga.size(); i++) {
 		    if (listaKnjiga.get(i).brojKnjige == brojKnjige) {
-			if (listaKnjiga.get(i).statusKnjige) {
-			    return true;
-			}
+		    	if (!listaKnjiga.get(i).isStatusKnjige()) {
+		    		return true;
+		    	}	
 		    }
 		}
 		return false;
 	}
 
-	
-	
-	public boolean provjeriValidnostKnjige(int brojKnjige) {
-		for(int i=0; i<listaKnjiga.size(); i++) {
-			if(!(listaKnjiga.get(i).statusKnjige)) {
-				System.out.println("Ova knjiga je vec izdata. Iznajmite neku drugu!");
-				return false;
+	public static void preuzetaKnjiga(int brojKnjige) { 
+		for (int i=0; i<listaKnjiga.size(); i++) {
+			if (listaKnjiga.get(i).brojKnjige == brojKnjige) {
+				listaKnjiga.get(i).setStatusKnjige(false);;
+				System.out.println("Knjiga broj: " + listaKnjiga.get(i).brojKnjige + " je preuzeta.");
 			}
 		}
-		return true;
 	}
-			
-	public static boolean provjeriPostojanjeKnjige(int brojKnjige) {
-		for(int i=0; i<listaKnjiga.size(); i++) {
-			if(listaKnjiga.get(i).brojKnjige != brojKnjige) {
-				System.out.println("Ova knjiga je nepostojeca (nema je u bazi)!");
-				return false;
-			}
-			else {
-				return true;
+	
+	public static void vracenaKnjiga(int brojKnjige) {
+		for (int i=0; i<listaKnjiga.size(); i++) {
+			if (listaKnjiga.get(i).brojKnjige == brojKnjige) {
+				listaKnjiga.get(i).setStatusKnjige(true);
+				System.out.println("Knjiga broj: " + listaKnjiga.get(i).brojKnjige + " je sada dostupna.");
 			}
 		}
-		
-		return true;
+	}
+	
+	public int getBrojKnjige() {
+		return brojKnjige;
+	}
+
+	public void setBrojKnjige(int brojKnjige) {
+		this.brojKnjige = brojKnjige;
+	}
+
+	public String getImeKnjige() {
+		return imeKnjige;
+	}
+
+	public void setImeKnjige(String imeKnjige) {
+		this.imeKnjige = imeKnjige;
+	}
+
+	public boolean isStatusKnjige() {
+		return statusKnjige;
+	}
+
+	public void setStatusKnjige(boolean statusKnjige) {
+		this.statusKnjige = statusKnjige;
 	}
 	
 }
